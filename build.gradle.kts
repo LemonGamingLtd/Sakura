39bba67ec9110310ad6a1e608827da4cc8232641
Samsuik <kfian294ma4@gmail.com>
2025-01-16 19:32:46 +0000

    All source patches applied and starts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     java
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.13"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.14"
 }
 
 paperweight {
@@ -26,11 +26,6 @@ paperweight {
             excludes = setOf("build.gradle.kts")
             patchesDir = file("$brand-api/paper-patches")
             outputDir = file("paper-api")
-        }
-        patchDir("paperApiGenerator") {
-            upstreamPath = "paper-api-generator"
-            patchesDir = file("$brand-api-generator/paper-patches")
-            outputDir = file("paper-api-generator")
         }
     }
 }
